2555b745cf079080aac04cfd5e32c247f53bcd44
Francisco Zambrano <136658451+Francisco-Zambrano@users.noreply.github.com>
2024-03-15 22:48:11 -0300

    segundo desafío

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-DESAFÍO ENTREGABLE - PROCESO DE TESTING
+DESAFÍO ENTREGABLE NÚMERO 1 - PROCESO DE TESTING
 
 Clases con ECMAScript y ECMAScript avanzado
 
@@ -15,3 +15,21 @@ Clases con ECMAScript y ECMAScript avanzado
 - Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
 - Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
 - Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
+
+DESAFÍO ENTREGABLE NÚMERO 2 - MANEJO DE ARCHIVOS
+
+- Realizar una clase de nombre “ProductManager”, el cual permitirá trabajar con múltiples productos. Éste debe poder agregar, consultar, modificar y eliminar un producto y manejarlo en persistencia de archivos (basado en entregable 1).
+- La clase debe contar con una variable this.path, el cual se inicializará desde el constructor y debe recibir la ruta a trabajar desde el momento de generar su instancia.
+- Debe guardar objetos con el siguiente formato:
+  id (se debe incrementar automáticamente, no enviarse desde el cuerpo)
+  title (nombre del producto)
+  description (descripción del producto)
+  price (precio)
+  thumbnail (ruta de imagen)
+  code (código identificador)
+  stock (número de piezas disponibles)
+- Debe tener un método addProduct el cual debe recibir un objeto con el formato previamente especificado, asignarle un id autoincrementable y guardarlo en el arreglo (recuerda siempre guardarlo como un array en el archivo).
+- Debe tener un método getProducts, el cual debe leer el archivo de productos y devolver todos los productos en formato de arreglo.
+- Debe tener un método getProductById, el cual debe recibir un id, y tras leer el archivo, debe buscar el producto con el id especificado y devolverlo en formato objeto.
+- Debe tener un método updateProduct, el cual debe recibir el id del producto a actualizar, así también como el campo a actualizar (puede ser el objeto completo, como en una DB), y debe actualizar el producto que tenga ese id en el archivo. NO DEBE BORRARSE SU ID
+- Debe tener un método deleteProduct, el cual debe recibir un id y debe eliminar el producto que tenga ese id en el archivo.
--- productManager.js
+++ productManager.js
@@ -1,52 +1,88 @@
+const fs = require('fs');
+const path = require('path');
+
 class ProductManager{
 
     #products;
+    #path;
     static idProduct = 0;
 
     constructor(){
-        this.#products = []
+        this.#path = path.resolve(__dirname, './data/products.json');
+        this.#products = this.#readListProducts();
+    }
+
+    #readListProducts(){
+        try{
+            if(fs.existsSync(this.#path))
+                return JSON.parse(fs.readFileSync(this.#path,'utf-8'));
+            return []; 
+        }catch(error){
+            console.log("Error reading file");
+        }
+    }
+
+    #saveNewData(){
+        try{
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products));
+        } catch(error){
+            console.log("Error saving file");
+        }
     }
 
     addProduct(title, description, price, thumbnail, code, stock){
-
         if(!title || !description || !price || !thumbnail || !code || !stock){
-            return "all parameters are required"
+            return "All parameters are required";
         }
-
+    
         const uniqueCode = this.#products.some(product => product.code === code);
         if(uniqueCode) {
-            return "Product Code Already Exist"
+            return "Product Code Already Exists";
         }
-
-        const id = ProductManager.idProduct = ProductManager.idProduct + 1;
-
-        const newProduct = {
-            id:id,
-            title:title,
-            description:description,
-            price:price,
-            thumbnail:thumbnail,
-            code:code,
-            stock:stock
-        };
-
+    
+        const lastId = this.#products.length > 0 ? this.#products[this.#products.length - 1].id : 0;
+        const id = lastId + 1;
+    
+        const newProduct = {id, title, description, price, thumbnail, code, stock};
         this.#products.push(newProduct);
-
-        return "Product successfully added"
-
+        this.#saveNewData();
+        return "Product successfully added";
     }
-
+    
     getProducts(){
-
         return this.#products;
-
     }
 
     getProductById(id){
-
         const product = this.#products.find(data => data.id == id);
         return product ? product : "Not Found";
+    }
 
+    updateProduct(id, objectUpdate){
+        let message = "The product does not exist"
+
+        const index = this.#products.findIndex(p=> p.id ===id);
+
+        if (index !== -1){
+            const {id, ...res} = objectUpdate;
+            this.#products[index] = {...this.#products[index], ...res}
+            this.#saveNewData();
+            message = "Updated Product";
+        }
+
+        return message;
+    } 
+
+    deleteProduct(id){
+        let message = "The product does not exist"
+
+        const index = this.#products.findIndex(p=> p.id === id);
+        if (index !== -1){
+            this.#products = this.#products.filter(p=> p.id !== id);
+            this.#saveNewData();
+            message = "Deleted Product"
+        } 
+        return message;
     }
 
 }

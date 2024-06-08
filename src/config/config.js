a94d6dfc02c7a08a4d226ecd0bfe44ed9d27a152
Francisco Zambrano <136658451+Francisco-Zambrano@users.noreply.github.com>
2024-06-08 14:27:08 -0300

    dotenv and commander fixed

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,25 @@
 import dotenv from "dotenv"
+import { Command, Option } from "commander";
+
+
+let program = new Command()
+
+program.addOption(new Option("-m, --mode <modo>", "script execution mode").choices(["dev", "prod"]).default("dev"))
+
+program.parse()
+const arg = program.opts()
+
+const mode = arg.mode
 
 dotenv.config(
     {
-        path: "./src/.env",
+        path: mode === "prod" ? "./src/.env.prduction" : "./src/.env.development",
         override: true
     }
 );
 
 export const config = {
+
     PORT: process.env.PORT,
     MONGO_URL: process.env.MONGO_URL,
     DB_NAME: process.env.DB_NAME,
@@ -15,4 +27,5 @@ export const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     CALLBACK_URL: process.env.CALLBACK_URL
+    
 };
